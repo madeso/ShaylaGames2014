a394334a4d058b55b7de3646bf06a98de5c6b6d4
Gustav <sir.gustav.the.coder@gmail.com>
2014-05-24 20:58:13 +0200

    Added credits

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.width = 800 -- t.screen.width in 0.8.0 and earlier
     t.window.height = 600 -- t.screen.height in 0.8.0 and earlier
-    t.window.title = "Master Bation" 
+    t.window.title = "Master Bation - @sirGustav - ShaylaGames 2014" 
 end
 
